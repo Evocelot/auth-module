89b259c1ddb5f976a1c6abe7ca2356400f9dd0ad
Danisovszky Márk <dmarkprog@gmail.com>
2024-08-28 07:38:10 +0200

    Fixin listing bug and permission update

--- auth-services/auth-service/src/main/java/hu/evocelot/auth/service/auth/interceptor/PermissionNeeded.java
+++ auth-services/auth-service/src/main/java/hu/evocelot/auth/service/auth/interceptor/PermissionNeeded.java
@@ -5,8 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import jakarta.interceptor.InterceptorBinding;
-
 /**
  * The annotation is intended to protect those REST methods that we want to secure. It allows defining the required permission, and before the method
  * execution, it checks whether the user attempting to use the function has the necessary permissions.
